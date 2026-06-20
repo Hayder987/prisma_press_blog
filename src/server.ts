7554eaba49695ad139d8ec80987ed825e41177aa
Hayder Ali <hayderbd4290@gmail.com>
2026-06-21 00:06:46 +0600

    server running successfully

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import { config } from "./config/env.config";
+import { prisma } from "./lib/prisma";
+
+const port = config.port || 5000;
+
+async function main (){
+    try {
+    await prisma.$connect();
+    console.log(`Prisma DB connection SuccessFully`);
+    app.listen(port, ()=>{
+        console.log(`Server Running At Port: ${port} SuccessFully`)
+    })
+     
+    } catch (error) {
+      console.log("server Error Found:", error);
+      await prisma.$disconnect();
+      process.exit(1)
+    }
+};
+
+main();

10483cb159966171b02095af2d3499165e3b2e53
Hayder Ali <hayderbd4290@gmail.com>
2026-06-22 01:29:51 +0600

    update root route response

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import { userRouter } from './modules/user/user.route';
+import { userRouter } from "./modules/user/user.route";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
 
-const app:Application = express();
+const app: Application = express();
 
 // using middleware
-app.use(cors({
-    origin : config.app_url,
-    credentials : true,
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // route middleware
@@ -21,9 +24,13 @@ app.use("/api/users", userRouter);
 
 
 // root route
-app.get("/", (req:Request, res:Response)=>{
-    res.send("Welcome to prisma press blogs")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Welcome to prisma press blogs",
+    data: [],
+  });
+});
 
 export default app;
-

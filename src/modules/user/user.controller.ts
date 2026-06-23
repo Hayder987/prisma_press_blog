0fd59de988ca39265662cce2ce5f3dbd1cce1ca9
Hayder Ali <hayderbd4290@gmail.com>
2026-06-23 19:54:04 +0600

    create catch async higher order function for error handleling

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,30 +1,25 @@
-import { Request, Response } from "express";
-import  httpStatus  from "http-status";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
 // register user
-const createUser = async(req:Request, res:Response)=>{
-  try {
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const body = req.body;
-    
+
     const result = await userService.createUserIntoDB(body);
 
     res.status(httpStatus.CREATED).json({
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: "User registered successfully",
-        data : result
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User registered successfully",
+      data: result,
     });
-    
-  } catch (error) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        message: (error as Error).message,
-    });
-  }
-};
+  },
+);
 
 export const userController = {
-    createUser
+  createUser,
 };

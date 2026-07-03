e2bf8de7965156f3f7bb62d78f2d72e9b1192f18
Hayder Ali <hayderbd4290@gmail.com>
2026-07-03 07:51:11 +0600

    create premium gurd for protact premium post

--- src/modules/premium/premium.route.ts
+++ src/modules/premium/premium.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { premiumController } from "./premium.controller";
+import { auth } from "../../middlewares/auth";
+import { Role } from "../../../generated/prisma/enums";
+import { premiumGuard } from "../../middlewares/premiumGurd";
 
 const router = Router();
 
-router.get("/posts", premiumController.getPremiumPosts);
-
+router.get(
+  "/posts",
+  auth(Role.ADMIN, Role.AUTHOR, Role.USER),
+  premiumGuard(),
+  premiumController.getPremiumPosts,
+);
 
 export const premiumRouter = router;
